40594b1b626dffa79b157d0c2ad9e27f938d38e7
mateus <mifh11@gmail.com>
2023-08-17 12:19:14 -0300

    a

---
+++ autounicycler.py
@@ -0,0 +1,53 @@
+import os
+import argparse
+import subprocess
+
+
+parser = argparse.ArgumentParser(
+    description='running autounicycler')
+
+parser.add_argument('-i', '--input', metavar='', type=str, required=True,
+                    help='Path to the files i.e: home/usr/Desktop/myfiles')
+parser.add_argument('-t', '--thread', metavar='', type=str, required=False,
+                    help='[OPTIONAL] Number of threads i.e: 8')
+
+args = parser.parse_args()
+
+diretorio = os.path.expanduser(f'{args.input}') 
+extensoes = ['.fastq', '.fq']
+
+def validate_input_directory(diretorio):
+    if not os.path.isdir(diretorio):
+        raise ValueError("Invalid input directory")
+
+def function(diretorio):
+    lista = []
+    if 'results_autounicycler' not in os.listdir(diretorio):
+        os.mkdir(os.path.join(f'{diretorio}','results_autounicycler'))
+    
+    for arquivos in os.listdir(args.input):
+        for extensao in extensoes:
+            if arquivos.endswith(extensao):   
+                lista.append(arquivos)
+                lista.sort()
+                break
+
+    for cada_item_da_lista in range(0, len(lista), 2):
+        lista2 = lista[cada_item_da_lista:cada_item_da_lista+2]   
+        
+        nome_do_diretorio = os.path.splitext(lista2[0])[0]
+         
+        os.mkdir(os.path.join(f'{diretorio}', 'results_autounicycler', f'{nome_do_diretorio}'))
+        caminho = os.path.join(f'{diretorio}', 'results_autounicycler', f'{nome_do_diretorio}')
+        
+        if args.thread:
+            command_line = ['unicycler', '-1', os.path.join(f'{diretorio}', f'{lista2[0]}'), '-2', os.path.join(f'{diretorio}', f'{lista2[1]}'), '-o', f'{caminho}', '-t', args.thread, '--min_fasta_length',	'200']
+            subprocess.call(command_line)
+            
+        else:
+            command_line = ['unicycler', '-1', os.path.join(f'{diretorio}', f'{lista2[0]}'), '-2', os.path.join(f'{diretorio}', f'{lista2[1]}'), '-o', f'{caminho}', '--min_fasta_length',	'200']
+            subprocess.call(command_line)
+            
+if __name__ == '__main__':
+    validate_input_directory(diretorio)
+    function(diretorio)
--- pandinha.py
+++ pandinha.py
@@ -8,12 +8,13 @@ def core_genome(df):
     core_genes = df[df['Presence Number'] == 11]
     genes = core_genes['Genes']
     
-    
+    #printando cada core    
     lista_core = []
     for i in genes:
         lista_core.append(i), ','
     print('a lista de genes core é: ', ','.join(lista_core))
 
+   #contando os genes core
     contador_core = 0
     for i in lista_core:
         contador_core += 1
@@ -23,12 +24,13 @@ def acessory_genome(df):
     acessory_genes = df[(df['Presence Number'] != 11) & (df['Presence Number'] != 1)]
     genes = acessory_genes['Genes']
     
-    
+    #printando cada acessory    
     lista_acessory = []
     for i in genes:
         lista_acessory.append(i), ','
     print('a lista de genes acessory é: ', ','.join(lista_acessory))
 
+   #contando os genes acessorios
     contador_acessory = 0
     for i in lista_acessory:
         contador_acessory += 1
@@ -38,26 +40,31 @@ def exclusive_genome(df):
     exclusive_genes = df[df['Presence Number'] == 1]   
     genes = exclusive_genes['Genes']
     
-    
+    #printando cada exclusive
     lista_exclusive = []
     for i in genes:
         lista_exclusive.append(i), ','
     print('a lista de genes exclusive é: ', ','.join(lista_exclusive))
-
+    
+    #contando os genes exclusivos
     contador_exclusive = 0
     for i in lista_exclusive:
         contador_exclusive += 1
     print('total de genes exclusive = ', contador_exclusive)
     
+    #procurando os genes no DB do panvita    
+    for cada_item in lista_exclusive:
+        print(cada_item)
+    
+exclusive_genome(df)
 
-def pan_analysis(df):
-    core_genome(df)
-    acessory_genome(df)
-    exclusive_genome(df)
+# def pan_analysis(df):
+#     core_genome(df)
+#     acessory_genome(df)
+#     exclusive_genome(df)    
     
-    
-if __name__ == '__main__':
-    pan_analysis(df)
+# if __name__ == '__main__':
+#     pan_analysis(df)
 
 
 
